9849cc84a589216bdc62b17b4ca1594008653f67
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-11-26 15:41:58 -0800

    Added rest of logic for operators with closures

--- StCalculator/CalculatorBrain.swift
+++ StCalculator/CalculatorBrain.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-func multiply(op1: Double, op2: Double) -> Double {
-    return op1 * op2
-}
 
 class CalculatorBrain {
     
@@ -24,9 +21,13 @@ class CalculatorBrain {
     var operations: Dictionary<String, Operation> = [
         "π": Operation.Constant(M_PI),
         "e": Operation.Constant(M_E),
+        "±": Operation.UnaryOperation({-$0}),
         "√": Operation.UnaryOperation(sqrt),
         "cos": Operation.UnaryOperation(cos),
-        "x": Operation.BinaryOperation(multiply),
+        "x": Operation.BinaryOperation({$0 * $1}),
+        "/": Operation.BinaryOperation({$0 / $1}),
+        "+": Operation.BinaryOperation({$0 + $1}),
+        "-": Operation.BinaryOperation({$0 - $1}),
         "=": Operation.Equals
     ]
     
@@ -44,14 +45,21 @@ class CalculatorBrain {
             switch operation {
             case .Constant(let value): accumulator = value
             case .UnaryOperation(let function): accumulator = function(accumulator)
-            case  .BinaryOperation(let function): pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
-            case .Equals: break
-            if pending != nil  {
-                accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
-                pending = nil
-                }
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+                
             }
             
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil  {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
         }
     }
     
